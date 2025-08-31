07945138d6fd0428b5a6945752a2d491f2e90748
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-31 22:11:12 +0530

    Update: Custom routes from user

--- src/Components/Params/Params.jsx
+++ src/Components/Params/Params.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 function Params() {
-  const {id} = useParams() //This is a router function not custom 
+  const {id} = useParams() //This is a router function, not custom 
   return (
     <div className='bg-blue-400 text-white  text-3xl p-4'>Params : {id}</div>
   )
