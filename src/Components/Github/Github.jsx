e03b89ad9af72b98f4147f515f7d2c492070e124
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-09-13 20:33:46 +0530

    few changes

--- src/Components/Github/Github.jsx
+++ src/Components/Github/Github.jsx
@@ -11,7 +11,7 @@ function Github() {
   },[])*/
   return (
     <div className='text-center m-4 bg-gray-700 text-white p-4 text-3xl'>Github followers: {data.followers}
-    <img src={data.avatar_url} alt='Git picture' width={300} />
+      <img src={data.avatar_url} alt='Git picture' width={300} />
     </div>
   )
 }
